b6679c888ef124f9a4fdfc9d5a3e9c080b016dfd
minochi11 <97766128+minochi11@users.noreply.github.com>
2023-08-07 22:03:12 +0700

    Update BENT2.js

--- BENT/BENT2.js
+++ BENT/BENT2.js
@@ -6,6 +6,7 @@
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/BENT/BENT2.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/BENT/BENT2.js
 // @author       You
+// @run-at       document-body
 // @match        http*://*/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant       GM.setValue
