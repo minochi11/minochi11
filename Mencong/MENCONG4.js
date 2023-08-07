c426eff3ce0551b55dee6c89a4dce648d3bfaaee
minochi11 <97766128+minochi11@users.noreply.github.com>
2023-08-07 23:34:05 +0700

    Update MENCONG4.js

--- Mencong/MENCONG4.js
+++ Mencong/MENCONG4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MNC4
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/Mencong/MENCONG4.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/Mencong/MENCONG4.js
