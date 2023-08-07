5ffc9b043050ae88c5bd0ec27dff81c59e71f6b0
minochi11 <97766128+minochi11@users.noreply.github.com>
2023-08-07 21:13:30 +0700

    Update CURUT2.js

--- CURUT/CURUT2.js
+++ CURUT/CURUT2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CURUT2
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/CURUT/CURUT2.js
 // @downloadURL   https://raw.githubusercontent.com/minochi11/minochi11/main/CURUT/CURUT2.js
