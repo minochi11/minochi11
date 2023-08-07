5218a541e529a0732ac846eec2e042b3f136a456
minochi11 <97766128+minochi11@users.noreply.github.com>
2023-08-07 22:07:52 +0700

    Update MANYUT4.js

--- Manyut/MANYUT4.js
+++ Manyut/MANYUT4.js
@@ -5,6 +5,7 @@
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/Manyut/MANYUT4.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/Manyut/MANYUT4.js
+// @run-at       document-body
 // @author       You
 // @match        http*://*/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
