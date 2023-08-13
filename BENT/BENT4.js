f164248fde0e701410802ffb3034fb6c79250462
minochi11 <97766128+minochi11@users.noreply.github.com>
2023-08-13 21:27:29 +0700

    Update BENT4.js

--- BENT/BENT4.js
+++ BENT/BENT4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BEN4
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  try to take over the world!
 // @author       You
 // @run-at       document-body
