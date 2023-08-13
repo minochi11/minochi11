2c0f25bb86b7b5b149ee7eea77a468cc8c3d59d8
minochi11 <97766128+minochi11@users.noreply.github.com>
2023-08-14 05:00:54 +0700

    Update BENT3.js

--- BENT/BENT3.js
+++ BENT/BENT3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BEN3
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.5
 // @description  try to take over the world!
 // @author       You
 // @run-at       document-body
