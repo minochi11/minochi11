8f1d193f2068b7609b9a241681c5b08e578fa7f8
minochi11 <97766128+minochi11@users.noreply.github.com>
2023-08-17 12:18:15 +0700

    Update CURUT1.js

--- CURUT V2/CURUT1.js
+++ CURUT V2/CURUT1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CURUT1
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/CURUT%20V2/CURUT1.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/CURUT%20V2/CURUT1.js
@@ -18,7 +18,7 @@
 
 
 
-var refresh = 100;
+var refresh = 10;
 
 
 var namagroup1 = "SHIOKELINCI4D";
