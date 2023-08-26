00835758b8e34ad7b673ff7028c70b42cb21ef40
minochi11 <97766128+minochi11@users.noreply.github.com>
2023-08-26 13:32:58 +0700

    Update NEW MENCONG1.js

--- MENCONG V2/NEW MENCONG1.js
+++ MENCONG V2/NEW MENCONG1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW MENCONG1
 // @namespace    http://tampermonkey.net/
-// @version      2.5
+// @version      2.6
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/MENCONG%20V2/NEW%20MENCONG1.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/MENCONG%20V2/NEW%20MENCONG1.js
