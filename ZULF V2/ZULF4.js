37e8a0b958390b084ad274de5808f350a72860c1
minochi11 <97766128+minochi11@users.noreply.github.com>
2023-09-02 13:47:28 +0700

    Update ZULF4.js

--- ZULF V2/ZULF4.js
+++ ZULF V2/ZULF4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW ZULF4
 // @namespace    http://tampermonkey.net/
-// @version      2.8
+// @version      2.9
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/ZULF%20V2/ZULF4.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/ZULF%20V2/ZULF4.js
@@ -19,49 +19,49 @@ var refresh = 90;
 
 
 var namagroup1 = 'HIGH4D';
-var Comment1 = 'SUNANFAIS = 46*86*39 #HIGH4D #TOGELHIGH4D';
+var Comment1 = 'SUNANFAIS = 46*86*39 #HIGH4D #TOGELHIGH4D'; 
 
 var namagroup2 = 'ANGKER';
-var Comment2 = 'ANGKER4D=FAISMU=71*33*15';
+var Comment2 = 'ANGKER4D=FAISMU=71*33*15'; 
 
 var namagroup3 = 'JNETOTO';
-var Comment3 = '#JNETOTO(FAZAMIR00)*96*14*13';
+var Comment3 = '#JNETOTO(FAZAMIR00)*96*14*13'; 
 
 var namagroup4 = 'GAIB4D';
-var Comment4 = 'GAIB4D=FAIZAL00=87*47*10';
+var Comment4 = 'GAIB4D=FAIZAL00=87*47*10'; 
 
-var namagroup5 = 'PORTOGEL';
-var Comment5 = '#PORTOGEL (FAISAMIR36) : 01*74*53';
+var namagroup5 = 'CAK4D';
+var Comment5 = '#CAK4D*FAISAMIR36*01*74*53'; 
 
 var namagroup6 = 'TiktakTogel';
-var Comment6 = '#Tiktaktogel / FANAFILL / 30 , 60 , 77';
+var Comment6 = '#Tiktaktogel / FANAFILL / 30 , 60 , 77'; 
 
 var namagroup7 = 'KHUSUS MACAU';
-var Comment7 = '#ASEPTOGEL FAISALAMIR12 71*33*15';
+var Comment7 = '#ASEPTOGEL FAISALAMIR12 71*33*15'; 
 
 var namagroup8 = 'BOCORAN PREDIKSI SDY';
-var Comment8 = '#TOGELKUY FAISMU 30*60*77';
+var Comment8 = '#TOGELKUY FAISMU 30*60*77'; 
 
 var namagroup9 = 'SHIOKELINCI4D';
-var Comment9 = '#shiokelinci4d*FASTBLUE1*01*74';
+var Comment9 = '#shiokelinci4d*FASTBLUE1*01*74'; 
 
 var namagroup10 = 'GILA4D';
-var Comment10 = 'GILA4D=FASTON11=87*47*10';
+var Comment10 = 'GILA4D=FASTON11=87*47*10'; 
 
 var namagroup11 = 'KEITOGEL';
-var Comment11 = '#keitogel = FASAKAL11 = 96*14*13';
+var Comment11 = '#keitogel = FASAKAL11 = 96*14*13'; 
 
 var namagroup12 = 'KiosToto';
-var Comment12 = 'KIOSTOTO=FAISALAMIR12=04*17*62';
+var Comment12 = 'KIOSTOTO=FAISALAMIR12=04*17*62'; 
 
 var namagroup13 = 'TOTOAKURAT';
-var Comment13 = '#TA*FAISALBA*46*86*39*';
+var Comment13 = '#TA*FAISALBA*46*86*39*'; 
 
-var namagroup14 = 'WAHANATOGEL';
-var Comment14 = '#WAHANATOGEL(FAISMU) : 65*16*06';
+var namagroup14 = 'RNR303';
+var Comment14 = '#RNR303(FAISMU) : 65*16*06'; 
 
 var namagroup15 = '12312312';
-var Comment15 = '';
+var Comment15 = ''; 
 
 var namagroup16 = '12312312';
 var Comment16 = '';
