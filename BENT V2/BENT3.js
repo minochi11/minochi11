2f44224f361c55e5b8fee2fcab8312a9f1e5335e
minochi11 <97766128+minochi11@users.noreply.github.com>
2023-09-07 00:33:10 +0700

    Update BENT3.js

--- BENT V2/BENT3.js
+++ BENT V2/BENT3.js
@@ -70,8 +70,8 @@ var Comment16 = '';
 
 
 /*======================================================================3D===============================================================*/
-var namagroup17 = 'BMWGROUP';
-var Comment17 = 'FALS4D/SARIAH99*973*362*807'; 
+var namagroup17 = 'BorToto';
+var Comment17 = '#Bortoto (SRITING81) : 973*362*807'; 
 
 var namagroup18 = 'Situstogel88';
 var Comment18 = '#SITUSTOGEL88 ( SIALANKU13 ) : 081*585*648'; 
