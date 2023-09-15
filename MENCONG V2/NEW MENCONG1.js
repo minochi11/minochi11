4ad98b3f88dff6bbf22f75c998b5b621c06d1ab0
minochi11 <97766128+minochi11@users.noreply.github.com>
2023-09-15 19:17:27 +0700

    Update NEW MENCONG1.js

--- MENCONG V2/NEW MENCONG1.js
+++ MENCONG V2/NEW MENCONG1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW MENCONG1
 // @namespace    http://tampermonkey.net/
-// @version      3.0
+// @version      3.1
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/MENCONG%20V2/NEW%20MENCONG1.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/MENCONG%20V2/NEW%20MENCONG1.js
@@ -54,8 +54,8 @@ var Comment11 = '#TA*MAUEWEK5X*53*41*98*';
 var namagroup12 = 'BOCORAN PREDIKSI SDY';
 var Comment12 = '#TOGELKUY MAKTUNG52 38*22*57'; 
 
-var namagroup13 = 'CAK4D';
-var Comment13 = '#CAK4D*MADUL45*62*15*95'; 
+var namagroup13 = 'TOYIBSLOT';
+var Comment13 = '#TOYIBSLOT ( MADUL45 ) : 62*15*95'; 
 
 var namagroup14 = 'GOSIPTOTO';
 var Comment14 = '#GOSIPTOTO (MASAK741) : *62*15*95'; 
