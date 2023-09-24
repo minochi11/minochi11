36f1c67ca1e9c9dd64a2d4dd901425a1cde6a8cd
minochi11 <97766128+minochi11@users.noreply.github.com>
2023-09-24 19:33:44 +0700

    Update BOS3.js

--- BOSQUE V2/BOS3.js
+++ BOSQUE V2/BOS3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BOS3
 // @namespace    http://tampermonkey.net/
-// @version      3.1
+// @version      3.2
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/BOSQUE%20V2/BOS3.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/BOSQUE%20V2/BOS3.js
@@ -71,7 +71,7 @@ var Comment16 = '';
 
 
 /*Keyword*/
-var keyword = ["room", "ROOM", "lomba","LOMBA","𝗥𝗢𝗢𝗠 𝗟𝗢𝗠𝗕𝗔","lomba angker4d","room lomba","ROOM_LOMBA","𝗥𝗢𝗢𝗠 𝗟𝗢𝗠𝗕𝗔","room lomba betting"];
+var keyword = ["room", "ROOM", "lomba","LOMBA","𝗥𝗢𝗢𝗠 𝗟𝗢𝗠𝗕𝗔","lomba angker4d","𝐋𝐎𝐌𝐁𝐀","ROOM_LOMBA","𝗥𝗢𝗢𝗠 𝗟𝗢𝗠𝗕𝗔","room lomba betting"];
 
 
 /*Backlist Kata*/
