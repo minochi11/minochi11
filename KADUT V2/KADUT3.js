47956c1d902836c5c6e7b22ecff0e419ab9142ec
minochi11 <97766128+minochi11@users.noreply.github.com>
2023-09-24 19:37:48 +0700

    Update KADUT3.js

--- KADUT V2/KADUT3.js
+++ KADUT V2/KADUT3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW KADUT3
 // @namespace    http://tampermonkey.net/
-// @version      3.2
+// @version      3.3
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/KADUT%20V2/KADUT3.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/KADUT%20V2/KADUT3.js
@@ -70,7 +70,7 @@ var Comment16 = '';
 
 
 /*======================================================================3D===============================================================*/
-var namagroup17 = 'BorToto';
+var namagroup17 = 'BORTOTO';
 var Comment17 = '#Bortoto (JOKILAN11) : 474*368*113'; 
 
 var namagroup18 = 'Situstogel88';
@@ -124,7 +124,7 @@ var Comment32 = 'PAKDE4D ( JOKILAN11 ) : 442*124*163';
 
 
 /*Keyword*/
-var keyword = ["room", "ROOM", "lomba","LOMBA","𝗥𝗢𝗢𝗠 𝗟𝗢𝗠𝗕𝗔","lomba angker4d","room lomba","ROOM_LOMBA","𝗥𝗢𝗢𝗠 𝗟𝗢𝗠𝗕𝗔","room lomba betting"];
+var keyword = ["room", "ROOM", "lomba","LOMBA","𝗥𝗢𝗢𝗠 𝗟𝗢𝗠𝗕𝗔","lomba angker4d","𝐋𝐎𝐌𝐁𝐀","ROOM_LOMBA","𝗥𝗢𝗢𝗠 𝗟𝗢𝗠𝗕𝗔","room lomba betting"];
 
 
 /*Backlist Kata*/
