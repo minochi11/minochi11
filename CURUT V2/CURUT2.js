a37d03d44691526c9eee1db4bf22812d92b6dd45
minochi11 <97766128+minochi11@users.noreply.github.com>
2023-09-25 02:30:20 +0700

    Update CURUT2.js

--- CURUT V2/CURUT2.js
+++ CURUT V2/CURUT2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW CURUT2
 // @namespace    http://tampermonkey.net/
-// @version      3.3
+// @version      3.4
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/CURUT%20V2/CURUT2.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/CURUT%20V2/CURUT2.js
@@ -22,8 +22,8 @@ var refresh = 90;
 var namagroup1 = 'SHIOKELINCI4D';
 var Comment1 = '#shiokelinci4d*MOCHOK52*18*53'; 
 
-var namagroup2 = 'FULUSTOTO';
-var Comment2 = '#FULUSTOTO*MOCHI92=26*40*92'; 
+var namagroup2 = 'WOLF138';
+var Comment2 = 'MOCHI92 = 26*40*92 #WOLF138 #TOGELWOLF138'; 
 
 var namagroup3 = 'TiktakTogel';
 var Comment3 = '#Tiktaktogel / MODAR17 / 11 , 39 , 85'; 
