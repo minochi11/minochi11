0c1ac67aa67113dae6e7e16f2a345680c138f8b0
minochi11 <97766128+minochi11@users.noreply.github.com>
2023-10-02 03:16:20 +0700

    Update KADUT2.js

--- KADUT V2/KADUT2.js
+++ KADUT V2/KADUT2.js
@@ -124,8 +124,7 @@ var Comment32 = 'PAKDE4D ( SRIDU268 ) : 822*009*366';
 
 
 /*Keyword*/
-var keyword = ["room", "ROOM", "lomba","LOMBA","𝗥𝗢𝗢𝗠 𝗟𝗢𝗠𝗕𝗔","lomba angker4d","𝐋𝐎𝐌𝐁𝐀","ROOM_LOMBA","𝗥𝗢𝗢𝗠 𝗟𝗢𝗠𝗕𝗔","room lomba betting"];
-
+var keyword = ["room", "ROOM", "lomba","LOMBA","𝗥𝗢𝗢𝗠","lomba angker4d","𝐋𝐎𝐌𝐁𝐀","ROOM_LOMBA","𝗟𝗢𝗠𝗕𝗔","room lomba betting"];
 
 /*Backlist Kata*/
 var Backlist = ["rekap", "artikel", "hasil","jam berapa","room lomba freebet","nanti","kapan","bos","mana"];
