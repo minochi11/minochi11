eb4dc80200d5b705a91fb8e479089aab0dc10ca1
minochi11 <97766128+minochi11@users.noreply.github.com>
2023-10-07 16:53:41 +0700

    Update NEW MENCONG4.js

--- MENCONG V2/NEW MENCONG4.js
+++ MENCONG V2/NEW MENCONG4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW MENCONG4
 // @namespace    http://tampermonkey.net/
-// @version      3.5
+// @version      3.8
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/MENCONG%20V2/NEW%20MENCONG4.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/MENCONG%20V2/NEW%20MENCONG4.js
@@ -58,8 +58,8 @@ var Comment12 = '#TOGELKUY TOMBIS34 93*77*07';
 var namagroup13 = 'TOYIBSLOT';
 var Comment13 = '#TOYIBSLOT ( TOMBOL24 ) : 46*50*05'; 
 
-var namagroup14 = 'GOSIPTOTO';
-var Comment14 = '#GOSIPTOTO (TOMBRENG34) : *46*50*05'; 
+var namagroup14 = 'IDTOGEL';
+var Comment14 = '#IDTOGEL*TOMBRENG34*46*50*05'; 
 
 var namagroup15 = 'SNIPER';
 var Comment15 = '#VESPATOGEL (TOMASKU34) 30*97*61'; 
@@ -72,10 +72,10 @@ var Comment16 = '';
 
 
 /*Keyword*/
-var keyword = ["room", "ROOM", "lomba","LOMBA","𝗥𝗢𝗢𝗠","lomba angker4d","𝐋𝐎𝐌𝐁𝐀","ROOM_LOMBA","𝗟𝗢𝗠𝗕𝗔","room lomba betting"];
+var keyword = ["ROOM", "𝗥𝗢𝗢𝗠", "LOMBA","𝗟𝗢𝗠𝗕𝗔","aawdaw","lomba angker4d","𝐋𝐎𝐌𝐁𝐀","ROOM_LOMBA","qweqw","room lomba betting"];
 
 /*Backlist Kata*/
-var Backlist = ["rekap", "artikel", "hasil","jam berapa","room lomba freebet","nanti","kapan","bos","mana"];
+var Backlist = ["rekap", "artikel", "hasil","jam berapa","room lomba freebet","nanti","kapan","cok","mana"];
 
 
 
@@ -153,18 +153,18 @@ GM.setValue("DONE", 0);
     var cekjam8 = document.getElementsByClassName('native-text')[27].textContent
     var cekjam9 = document.getElementsByClassName('native-text')[27].textContent
 
-    var cekcomment1 = document.getElementsByClassName('native-text')[20].textContent.toLowerCase();
-    var cekcomment2 = document.getElementsByClassName('native-text')[21].textContent.toLowerCase();
-    var cekcomment3 = document.getElementsByClassName('native-text')[22].textContent.toLowerCase();
-    var cekcomment4 = document.getElementsByClassName('native-text')[23].textContent.toLowerCase();
-    var cekcomment5 = document.getElementsByClassName('native-text')[24].textContent.toLowerCase();
-    var cekcomment6 = document.getElementsByClassName('native-text')[25].textContent.toLowerCase();
-    var cekcomment7 = document.getElementsByClassName('native-text')[26].textContent.toLowerCase();
-    var cekcomment8 = document.getElementsByClassName('native-text')[27].textContent.toLowerCase();
-    var cekcomment9 = document.getElementsByClassName('native-text')[28].textContent.toLowerCase();
-    var cekcomment10 = document.getElementsByClassName('native-text')[29].textContent.toLowerCase();
-    var cekcomment11= document.getElementsByClassName('native-text')[30].textContent.toLowerCase();
-    var cekcomment12= document.getElementsByClassName('native-text')[31].textContent.toLowerCase();
+    var cekcomment1 = document.getElementsByClassName('native-text')[20].textContent;
+    var cekcomment2 = document.getElementsByClassName('native-text')[21].textContent;
+    var cekcomment3 = document.getElementsByClassName('native-text')[22].textContent;
+    var cekcomment4 = document.getElementsByClassName('native-text')[23].textContent;
+    var cekcomment5 = document.getElementsByClassName('native-text')[24].textContent;
+    var cekcomment6 = document.getElementsByClassName('native-text')[25].textContent;
+    var cekcomment7 = document.getElementsByClassName('native-text')[26].textContent;
+    var cekcomment8 = document.getElementsByClassName('native-text')[27].textContent;
+    var cekcomment9 = document.getElementsByClassName('native-text')[28].textContent;
+    var cekcomment10 = document.getElementsByClassName('native-text')[29].textContent;
+    var cekcomment11= document.getElementsByClassName('native-text')[30].textContent;
+    var cekcomment12= document.getElementsByClassName('native-text')[31].textContent;
 
     var validcom = false;
     var valid = false;
