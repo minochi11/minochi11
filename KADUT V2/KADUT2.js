fb3035ab2ba2989ec01c216f1aadc1c5b11dad13
minochi11 <97766128+minochi11@users.noreply.github.com>
2023-10-07 16:40:17 +0700

    Update KADUT2.js

--- KADUT V2/KADUT2.js
+++ KADUT V2/KADUT2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW KADUT2
 // @namespace    http://tampermonkey.net/
-// @version      3.7
+// @version      3.8
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/KADUT%20V2/KADUT2.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/KADUT%20V2/KADUT2.js
@@ -58,8 +58,8 @@ var Comment12 = '#TOGELKUY SRISUWING 93*81*45';
 var namagroup13 = 'TOYIBSLOT';
 var Comment13 = '#TOYIBSLOT ( SRIMBELEWEL ) : 98*95*76'; 
 
-var namagroup14 = 'GOSIPTOTO';
-var Comment14 = '#GOSIPTOTO (SRITILKON) : *93*81*45'; 
+var namagroup14 = 'IDTOGEL';
+var Comment14 = '#IDTOGEL*SRITILKON*93*81*45'; 
 
 var namagroup15 = 'SNIPER';
 var Comment15 = '#VESPATOGEL (SRILAKCU) 75*07*04'; 
@@ -93,8 +93,8 @@ var Comment23 = '#DADU4D (522*862*928) SRIWING6';
 var namagroup24 = 'OTU SLOT';
 var Comment24 = '#OTUSLOT*SRIWING6*790*584*956*BETTING'; 
 
-var namagroup25 = 'PASCOL4D';
-var Comment25 = '#PASCOL4D (658*675*041) = SRILAK**'; 
+var namagroup25 = 'ODENGTOTO';
+var Comment25 = '#ODENGTOTO (SRILAKCU) = 658*675*041'; 
 
 var namagroup26 = 'LINGTOGEL77';
 var Comment26 = '#LINGTOGEL77*SRIWING67*949*984*887'; 
@@ -124,10 +124,11 @@ var Comment32 = '#PUCUK4D (SRIDU2**) = 283*860*341';
 
 
 /*Keyword*/
-var keyword = ["room", "ROOM", "lomba","LOMBA","𝗥𝗢𝗢𝗠","lomba angker4d","𝐋𝐎𝐌𝐁𝐀","ROOM_LOMBA","𝗟𝗢𝗠𝗕𝗔","room lomba betting"];
+var keyword = ["ROOM", "𝗥𝗢𝗢𝗠", "LOMBA","𝗟𝗢𝗠𝗕𝗔","aawdaw","lomba angker4d","𝐋𝐎𝐌𝐁𝐀","ROOM_LOMBA","qweqw","room lomba betting"];
 
 /*Backlist Kata*/
-var Backlist = ["rekap", "artikel", "hasil","jam berapa","room lomba freebet","nanti","kapan","bos","mana"];
+var Backlist = ["rekap", "artikel", "hasil","jam berapa","room lomba freebet","nanti","kapan","cok","mana"];
+
 
 
 
@@ -209,18 +210,18 @@ GM.setValue("DONE", 0);
     var cekjam8 = document.getElementsByClassName('native-text')[27].textContent
     var cekjam9 = document.getElementsByClassName('native-text')[27].textContent
 
-    var cekcomment1 = document.getElementsByClassName('native-text')[20].textContent.toLowerCase();
-    var cekcomment2 = document.getElementsByClassName('native-text')[21].textContent.toLowerCase();
-    var cekcomment3 = document.getElementsByClassName('native-text')[22].textContent.toLowerCase();
-    var cekcomment4 = document.getElementsByClassName('native-text')[23].textContent.toLowerCase();
-    var cekcomment5 = document.getElementsByClassName('native-text')[24].textContent.toLowerCase();
-    var cekcomment6 = document.getElementsByClassName('native-text')[25].textContent.toLowerCase();
-    var cekcomment7 = document.getElementsByClassName('native-text')[26].textContent.toLowerCase();
-    var cekcomment8 = document.getElementsByClassName('native-text')[27].textContent.toLowerCase();
-    var cekcomment9 = document.getElementsByClassName('native-text')[28].textContent.toLowerCase();
-    var cekcomment10 = document.getElementsByClassName('native-text')[29].textContent.toLowerCase();
-    var cekcomment11= document.getElementsByClassName('native-text')[30].textContent.toLowerCase();
-    var cekcomment12= document.getElementsByClassName('native-text')[31].textContent.toLowerCase();
+    var cekcomment1 = document.getElementsByClassName('native-text')[20].textContent;
+    var cekcomment2 = document.getElementsByClassName('native-text')[21].textContent;
+    var cekcomment3 = document.getElementsByClassName('native-text')[22].textContent;
+    var cekcomment4 = document.getElementsByClassName('native-text')[23].textContent;
+    var cekcomment5 = document.getElementsByClassName('native-text')[24].textContent;
+    var cekcomment6 = document.getElementsByClassName('native-text')[25].textContent;
+    var cekcomment7 = document.getElementsByClassName('native-text')[26].textContent;
+    var cekcomment8 = document.getElementsByClassName('native-text')[27].textContent;
+    var cekcomment9 = document.getElementsByClassName('native-text')[28].textContent;
+    var cekcomment10 = document.getElementsByClassName('native-text')[29].textContent;
+    var cekcomment11= document.getElementsByClassName('native-text')[30].textContent;
+    var cekcomment12= document.getElementsByClassName('native-text')[31].textContent;
 
     var validcom = false;
     var valid = false;
