25c2adcd696276313b56e14b7416a4123ae17f83
minochi11 <97766128+minochi11@users.noreply.github.com>
2023-10-09 23:21:07 +0700

    Update BENT1.js

--- BENT V2/BENT1.js
+++ BENT V2/BENT1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW BENT1
 // @namespace    http://tampermonkey.net/
-// @version      3.8
+// @version      3.9
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/BENT%20V2/BENT1.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/BENT%20V2/BENT1.js
@@ -130,7 +130,7 @@ var Comment32 = '#PUCUK4D (DONAL**) = 121*290*397';
 var keyword = ["ROOM", "𝗥𝗢𝗢𝗠", "LOMBA","𝗟𝗢𝗠𝗕𝗔","aawdaw","lomba angker4d","𝐋𝐎𝐌𝐁𝐀","ROOM_LOMBA","qweqw","room lomba betting"];
 
 /*Backlist Kata*/
-var Backlist = ["rekap", "artikel", "hasil","jam berapa","room lomba freebet","nanti","kapan","cok","mana"];
+var Backlist = ["REKAP", "rekap", "hasil","HASIL","ROOM LOMBA FREEBET","nanti","kapan","Rekap","Hasil"];
 
 
 
