a8b758b9008bbf3857d08de0403913fc0fcd57a7
minochi11 <97766128+minochi11@users.noreply.github.com>
2023-10-12 15:08:56 +0700

    Update ZULF1.js

--- ZULF V2/ZULF1.js
+++ ZULF V2/ZULF1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW ZULF1
 // @namespace    http://tampermonkey.net/
-// @version      3.10
+// @version      3.11
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/ZULF%20V2/ZULF1.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/ZULF%20V2/ZULF1.js
@@ -60,8 +60,8 @@ var Comment13 = '#TA*ZULFANI*26*27*29*';
 var namagroup14 = 'IDTOGEL';
 var Comment14 = '#IDTOGEL*ZULAFRNA002*89*03*41'; 
 
-var namagroup15 = '123123123';
-var Comment15 = ''; 
+var namagroup15 = 'DENTOTO';
+var Comment15 = '#DENTOTO 89*03*41 ( ZUZU34 )'; 
 
 var namagroup16 = '123213213';
 var Comment16 = '123123';
@@ -150,7 +150,7 @@ GM.setValue("DONE", 0);
     var cekjam6 = document.getElementsByClassName('native-text')[25].textContent
     var cekjam7 = document.getElementsByClassName('native-text')[26].textContent
     var cekjam8 = document.getElementsByClassName('native-text')[27].textContent
-    var cekjam9 = document.getElementsByClassName('native-text')[27].textContent
+    var cekjam9 = document.getElementsByClassName('native-text')[28].textContent
 
    var cekcomment1 = document.getElementsByClassName('native-text')[20].textContent;
     var cekcomment2 = document.getElementsByClassName('native-text')[21].textContent;
