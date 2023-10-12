274829c5e50a87c94e711c22a684447c9f5d0979
minochi11 <97766128+minochi11@users.noreply.github.com>
2023-10-12 15:02:36 +0700

    Update NEW MENCONG4.js

--- MENCONG V2/NEW MENCONG4.js
+++ MENCONG V2/NEW MENCONG4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW MENCONG4
 // @namespace    http://tampermonkey.net/
-// @version      3.10
+// @version      3.11
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/MENCONG%20V2/NEW%20MENCONG4.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/MENCONG%20V2/NEW%20MENCONG4.js
@@ -64,8 +64,8 @@ var Comment14 = '#IDTOGEL*TOMBRENG34*46*50*05';
 var namagroup15 = 'SNIPER';
 var Comment15 = '#VESPATOGEL (TOMASKU34) 30*97*61'; 
 
-var namagroup16 = '123123123';
-var Comment16 = '';
+var namagroup16 = 'DENTOTO';
+var Comment16 = '#DENTOTO 46*50*05 ( TOMASKU34 )';
 
 
 
@@ -151,7 +151,7 @@ GM.setValue("DONE", 0);
     var cekjam6 = document.getElementsByClassName('native-text')[25].textContent
     var cekjam7 = document.getElementsByClassName('native-text')[26].textContent
     var cekjam8 = document.getElementsByClassName('native-text')[27].textContent
-    var cekjam9 = document.getElementsByClassName('native-text')[27].textContent
+    var cekjam9 = document.getElementsByClassName('native-text')[28].textContent
 
     var cekcomment1 = document.getElementsByClassName('native-text')[20].textContent;
     var cekcomment2 = document.getElementsByClassName('native-text')[21].textContent;
