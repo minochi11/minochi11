4abf3bb7966032ef0adee6b63a6c727403917ebb
minochi11 <97766128+minochi11@users.noreply.github.com>
2023-10-12 14:55:42 +0700

    Update KADUT3.js

--- KADUT V2/KADUT3.js
+++ KADUT V2/KADUT3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW KADUT3
 // @namespace    http://tampermonkey.net/
-// @version      3.11
+// @version      3.12
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/KADUT%20V2/KADUT3.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/KADUT%20V2/KADUT3.js
@@ -64,8 +64,8 @@ var Comment14 = '#IDTOGEL*JOVINDUL*70*31*86';
 var namagroup15 = 'SNIPER';
 var Comment15 = '#VESPATOGEL (JOROKLUH) 32*28*01'; 
 
-var namagroup16 = '12312312';
-var Comment16 = '';
+var namagroup16 = 'DENTOTO';
+var Comment16 = '#DENTOTO 70*31*86 ( JOROKLUH )';
 
 
 
@@ -210,7 +210,7 @@ GM.setValue("DONE", 0);
     var cekjam6 = document.getElementsByClassName('native-text')[25].textContent
     var cekjam7 = document.getElementsByClassName('native-text')[26].textContent
     var cekjam8 = document.getElementsByClassName('native-text')[27].textContent
-    var cekjam9 = document.getElementsByClassName('native-text')[27].textContent
+    var cekjam9 = document.getElementsByClassName('native-text')[28].textContent
 
     var cekcomment1 = document.getElementsByClassName('native-text')[20].textContent;
     var cekcomment2 = document.getElementsByClassName('native-text')[21].textContent;
