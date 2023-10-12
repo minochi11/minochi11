738486808ccfd014ad603c808cd6b37a57f1f667
minochi11 <97766128+minochi11@users.noreply.github.com>
2023-10-12 14:58:04 +0700

    Update MANYUT2.js

--- MANYUT V2/MANYUT2.js
+++ MANYUT V2/MANYUT2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW MANYUT2
 // @namespace    http://tampermonkey.net/
-// @version      3.11
+// @version      3.12
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/MANYUT%20V2/MANYUT2.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/MANYUT%20V2/MANYUT2.js
@@ -64,8 +64,8 @@ var Comment14 = '#IDTOGEL*EKOJAY52*72*57*76';
 var namagroup15 = 'SNIPER';
 var Comment15 = '#VESPATOGEL (EKOWIJAY1) 29*82*83'; 
 
-var namagroup16 = '1231231';
-var Comment16 = '';
+var namagroup16 = 'DENTOTO';
+var Comment16 = '#DENTOTO 72*57*76 ( EKOR665 )';
 
 /*======================================================================3D===============================================================*/
 var namagroup17 = 'BORTOTO';
@@ -206,7 +206,7 @@ GM.setValue("DONE", 0);
     var cekjam6 = document.getElementsByClassName('native-text')[25].textContent
     var cekjam7 = document.getElementsByClassName('native-text')[26].textContent
     var cekjam8 = document.getElementsByClassName('native-text')[27].textContent
-    var cekjam9 = document.getElementsByClassName('native-text')[27].textContent
+    var cekjam9 = document.getElementsByClassName('native-text')[28].textContent
 
     var cekcomment1 = document.getElementsByClassName('native-text')[20].textContent;
     var cekcomment2 = document.getElementsByClassName('native-text')[21].textContent;
