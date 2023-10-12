989a1df9731e557e6b98e6be6d7c9d483f24ee60
minochi11 <97766128+minochi11@users.noreply.github.com>
2023-10-12 14:41:15 +0700

    Update KADUT1.js

--- KADUT V2/KADUT1.js
+++ KADUT V2/KADUT1.js
@@ -73,7 +73,7 @@ var Comment16 = '';
 var namagroup17 = 'BORTOTO';
 var Comment17 = 'BORTOTO (BOLEN19) *561*133*242'; 
 
-var namagroup18 = 'CIKA4D';
+var namagroup18 = 'CIKA';
 var Comment18 = '#CIKA4D*BUNCIT17*629*282*409'; 
 
 var namagroup19 = 'DETIKGROUP';
