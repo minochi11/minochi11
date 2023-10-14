747004f74045f3af42f83284bde6b6d3639daf0e
minochi11 <97766128+minochi11@users.noreply.github.com>
2023-10-14 18:47:25 +0700

    Update BENT4.js

--- BENT V2/BENT4.js
+++ BENT V2/BENT4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW BENT4
 // @namespace    http://tampermonkey.net/
-// @version      3.12
+// @version      3.13
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/BENT%20V2/BENT4.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/BENT%20V2/BENT4.js
@@ -116,7 +116,7 @@ var namagroup31 = 'BROMOTOTO';
 var Comment31 = 'BOLER19 # 977/291/211 #YOGAPRO'; 
 
 var namagroup32 = 'PUCUK4D';
-var Comment32 = '#PUCUK4D (BONES17) = 371*004*460';
+var Comment32 = '#PUCUK4D (BONES**) = 371*004*460';
 
 
 
