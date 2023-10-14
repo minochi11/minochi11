f383d222bebc45d0742eb791db29021f94eb8c95
minochi11 <97766128+minochi11@users.noreply.github.com>
2023-10-14 18:49:15 +0700

    Update MANYUT3.js

--- MANYUT V2/MANYUT3.js
+++ MANYUT V2/MANYUT3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW MANYUT3
 // @namespace    http://tampermonkey.net/
-// @version      3.12
+// @version      3.13
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/MANYUT%20V2/MANYUT3.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/MANYUT%20V2/MANYUT3.js
@@ -114,7 +114,7 @@ var namagroup31 = 'BROMOTOTO';
 var Comment31 = 'ALAHMBUH # 785/772/583 #YOGAPRO'; 
 
 var namagroup32 = 'PUCUK4D';
-var Comment32 = '#PUCUK4D (ALTIMBUL) = 132*138*846';
+var Comment32 = '#PUCUK4D (ALTIMB**) = 132*138*846';
 
 
 
