64245cb23f5c215e75579690b90bb7cd534276b1
minochi11 <97766128+minochi11@users.noreply.github.com>
2023-10-17 12:19:09 +0700

    Update BENT1.js

--- BENT V2/BENT1.js
+++ BENT V2/BENT1.js
@@ -147,13 +147,14 @@ var jam9 = "8 menit"
 
 var jam = [jam1,jam2,jam3,jam4,jam5,jam6,jam7,jam8,jam9];
 
-setInterval(function(){
-
-
 var pasaran1 = "SYDNEY"
 var pasaran2 = "SINGAPORE"
 var pasaran3 = "HONGKONG"
 var commentpasaran = ""
+setInterval(function(){
+
+
+
 var today = new Date().getHours();
 if (today >= 7 && today <= 11) {
    commentpasaran = pasaran1
