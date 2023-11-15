d60acf0fd1141c4d7f2528c7395af3e95b5a714f
minochi11 <97766128+minochi11@users.noreply.github.com>
2023-11-16 00:30:46 +0700

    Update KADUT4.js

--- KADUT V2/KADUT4.js
+++ KADUT V2/KADUT4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW KADUT4
 // @namespace    http://tampermonkey.net/
-// @version      3.20
+// @version      3.21
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/KADUT%20V2/KADUT4.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/KADUT%20V2/KADUT4.js
@@ -105,17 +105,17 @@ var Comment27 = '#ALEXISTOGEL(SANGEKABEH) 3D = 937 , 249 , 354';
 var namagroup28 = 'LOMBA AMANAH';
 var Comment28 = '#GTATOGEL {SAWERBOSE0} = 537*574*097'; 
 
-var namagroup29 = 'CAPTOGEL';
-var Comment29 = '#CAPTOGEL (SAWERSEK)=*287*074*184'; 
+var namagroup29 = 'PBOTOTO';
+var Comment29 = 'PBOTOTO 287*074*184 ( SAWERSEK )'; 
 
-var namagroup30 = 'TRIBUN';
-var Comment30 = '#TRIBUNTOGEL = (SAWERBOSE0) = 802*938*832'; 
+var namagroup30 = 'AWAL';
+var Comment30 = '#AWALSLOT 802*938*832 ( SAWERBOSE0 )'; 
 
 var namagroup31 = 'PREDIKSI TOTO';
 var Comment31 = 'SALAMANAE # 854/687/353 #YOGAPRO'; 
 
 var namagroup32 = 'PUCUK4D';
-var Comment32 = '#PUCUK4D (SAKEMP**) = 347*717*349';
+var Comment32 = '#PUCUK4D (SAKEMPUL) = 347*717*349';
 
 
 
