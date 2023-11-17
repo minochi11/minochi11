426fb8b1de2a56b952d22c8acded72a0e308f2fa
minochi11 <97766128+minochi11@users.noreply.github.com>
2023-11-17 21:42:24 +0700

    Update KADUT2.js

--- KADUT V2/KADUT2.js
+++ KADUT V2/KADUT2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW KADUT2
 // @namespace    http://tampermonkey.net/
-// @version      3.22
+// @version      3.24
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/KADUT%20V2/KADUT2.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/KADUT%20V2/KADUT2.js
