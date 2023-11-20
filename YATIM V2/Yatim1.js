43e5c96a55588db911552586a64887a516e3fbc4
minochi11 <97766128+minochi11@users.noreply.github.com>
2023-11-20 22:41:32 +0700

    Update Yatim1.js

--- YATIM V2/Yatim1.js
+++ YATIM V2/Yatim1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Yatim1
 // @namespace    http://tampermonkey.net/
-// @version      3.16
+// @version      3.17
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/YATIM%20V2/Yatim1.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/YATIM%20V2/Yatim1.js
@@ -21,8 +21,8 @@ var refresh = 90;
 var namagroup1 = 'SHIOKELINCI4D';
 var Comment1 = '#shiokelinci4d*GUNGJI1*66*72'; 
 
-var namagroup2 = 'VELBET4D';
-var Comment2 = '{GUNGHOK4} = #VELBET4D = 76*50*10'; 
+var namagroup2 = 'PREDIKSI SYAIR';
+var Comment2 = '#IYATOTO GUNGHOK4 76*50*10'; 
 
 var namagroup3 = 'TiktakTogel';
 var Comment3 = '#Tiktaktogel / GUNGHJ6 / 79 , 02 , 77'; 
