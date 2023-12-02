b60a05563a41fbfc0d88cd020b2f707df61fedf9
minochi11 <97766128+minochi11@users.noreply.github.com>
2023-12-02 23:23:08 +0700

    Update BENT3.js

--- BENT V2/BENT3.js
+++ BENT V2/BENT3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW BENT3
 // @namespace    http://tampermonkey.net/
-// @version      3.30
+// @version      3.31
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/BENT%20V2/BENT3.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/BENT%20V2/BENT3.js
@@ -108,14 +108,14 @@ var Comment28 = '#GTATOGEL {CACINGAN54} = 936*352*895';
 var namagroup29 = 'PBOTOTO';
 var Comment29 = 'PBOTOTO 768*092*870 ( SINTADIA18 )'; 
 
-var namagroup30 = 'GOJO4D'
+var namagroup30 = 'NAKAMA'
 var Comment30 = 'GOJO4D ( SIALANKU13 ) : 926*217*961'; 
 
 var namagroup31 = 'PREDIKSI TOTO';
 var Comment31 = 'SIMBOLU26 # 635/103/883 #YOGAPRO'; 
 
-var namagroup32 = 'PUCUK4D';
-var Comment32 = '#PUCUK4D (SRITING**) = 189*414*986';
+var namagroup32 = 'KITAB4D';
+var Comment32 = '{SRITING81} = 189*414*986 #AGENTOGELKITAB4D #DAFTARTOGELKITAB4D';
 
 
 
