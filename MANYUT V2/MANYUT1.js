f545d2b6c9eeef9f82beb9fa0cbc3b511cf5be18
minochi11 <97766128+minochi11@users.noreply.github.com>
2023-12-14 23:53:06 +0700

    Update MANYUT1.js

--- MANYUT V2/MANYUT1.js
+++ MANYUT V2/MANYUT1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW MANYUT1
 // @namespace    http://tampermonkey.net/
-// @version      3.34
+// @version      3.35
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/MANYUT%20V2/MANYUT1.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/MANYUT%20V2/MANYUT1.js
@@ -32,7 +32,7 @@ var namagroup4 = 'GAIB4D';
 var Comment4 = 'GAIB4D=HONDANG41=60*80*79'; 
 
 var namagroup5 = 'KEITOGEL';
-var Comment5 = '#keitogel = HOMOGEN12 = 42*74*07'; 
+var Comment5 = '#keitogel = (HOMOGEN12) = 42*74*07'; 
 
 var namagroup6 = 'KiosToto';
 var Comment6 = 'KIOSTOTO=HOPENG99=27*26*47'; 
