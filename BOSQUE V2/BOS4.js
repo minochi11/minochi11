15e82f28aef98f8ef69707ad32265f4c7ad8ba9e
minochi11 <97766128+minochi11@users.noreply.github.com>
2023-12-14 23:49:59 +0700

    Update BOS4.js

--- BOSQUE V2/BOS4.js
+++ BOSQUE V2/BOS4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BOS4
 // @namespace    http://tampermonkey.net/
-// @version      3.17
+// @version      3.18
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/BOSQUE%20V2/BOS4.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/BOSQUE%20V2/BOS4.js
@@ -31,7 +31,7 @@ var namagroup4 = 'GAIB4D';
 var Comment4 = 'GAIB4D=ZORAN36=30*45*36'; 
 
 var namagroup5 = 'KEITOGEL';
-var Comment5 = '#keitogel = ZONIAK63 = 25*54*74'; 
+var Comment5 = '#keitogel = (ZONIAK63) = 25*54*74'; 
 
 var namagroup6 = 'KiosToto';
 var Comment6 = 'KIOSTOTO=ZOMING82=44*94*05'; 
