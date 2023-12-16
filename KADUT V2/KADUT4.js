56714b7d55921bae3fa0225d8934504802d2b7f6
minochi11 <97766128+minochi11@users.noreply.github.com>
2023-12-16 18:58:51 +0700

    Update KADUT4.js

--- KADUT V2/KADUT4.js
+++ KADUT V2/KADUT4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW KADUT4
 // @namespace    http://tampermonkey.net/
-// @version      3.35
+// @version      3.36
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/KADUT%20V2/KADUT4.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/KADUT%20V2/KADUT4.js
@@ -100,7 +100,7 @@ var namagroup26 = 'LING77';
 var Comment26 = '#LINGTOGEL77*SAIKIPIYE*919*816*380'; 
 
 var namagroup27 = 'ALEXISTOGEL';
-var Comment27 = '#ALEXISTOGEL(SANGEKABEH) 3D = 937 , 249 , 354'; 
+var Comment27 = '#ALEXISTOGEL(SANGEKABEH) 3D = 937*249*354'; 
 
 var namagroup28 = 'LOMBA AMANAH';
 var Comment28 = '#GTATOGEL {SAWERBOSE0} = 537*574*097'; 
