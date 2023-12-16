16c9eddc1041aea8e0be939dcf597788052f6562
minochi11 <97766128+minochi11@users.noreply.github.com>
2023-12-16 19:00:47 +0700

    Update MANYUT2.js

--- MANYUT V2/MANYUT2.js
+++ MANYUT V2/MANYUT2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW MANYUT2
 // @namespace    http://tampermonkey.net/
-// @version      3.35
+// @version      3.36
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/MANYUT%20V2/MANYUT2.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/MANYUT%20V2/MANYUT2.js
@@ -99,7 +99,7 @@ var namagroup26 = 'LING77';
 var Comment26 = '#LINGTOGEL77*MOSIOM19*076*944*027'; 
 
 var namagroup27 = 'ALEXISTOGEL';
-var Comment27 = '#ALEXISTOGEL(MODAR17) 3D = 593 , 670 , 575'; 
+var Comment27 = '#ALEXISTOGEL(MODAR17) 3D = 593*670*575'; 
 
 var namagroup28 = 'LOMBA AMANAH';
 var Comment28 = '#GTATOGEL {MORAK16} = 311*695*851'; 
