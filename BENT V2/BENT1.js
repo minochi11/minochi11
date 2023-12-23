79003ba4ddadfd2e6c0297cb6548f12a3a18f01b
minochi11 <97766128+minochi11@users.noreply.github.com>
2023-12-23 18:40:46 +0700

    Update BENT1.js

--- BENT V2/BENT1.js
+++ BENT V2/BENT1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW BENT1
 // @namespace    http://tampermonkey.net/
-// @version      3.36
+// @version      3.37
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/BENT%20V2/BENT1.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/BENT%20V2/BENT1.js
@@ -93,7 +93,7 @@ var namagroup23 = 'ABUTOGEL';
 var Comment23 = '#ABUTOGEL*DOPLOSU1*730*146*072'; 
 
 var namagroup24 = 'OTU SLOT';
-var Comment24 = '#OTUSLOT*ASIMATJUMBO*031*231*507*BETTING'; 
+var Comment24 = '#OTUSLOT*ASIMATJUMBO*031*231*BETTING'; 
 
 var namagroup25 = 'BUSURGROUP';
 var Comment25 = commentpasar('#KAPAK4D*DAWET121*597*762*443'); 
