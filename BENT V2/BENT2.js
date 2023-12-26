c768c846afd6df053e3e7868f572d54295d3b6dc
minochi11 <97766128+minochi11@users.noreply.github.com>
2023-12-26 21:55:06 +0700

    Update BENT2.js

--- BENT V2/BENT2.js
+++ BENT V2/BENT2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW BENT2
 // @namespace    http://tampermonkey.net/
-// @version      3.37
+// @version      3.38
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/BENT%20V2/BENT2.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/BENT%20V2/BENT2.js
@@ -70,8 +70,8 @@ var Comment16 = '#IYATOTO BIMBO91 36*25*26';
 
 
 /*======================================================================3D===============================================================*/
-var namagroup17 = 'Pecah77';
-var Comment17 = '#PECAH77 (POLIT**) = 652*375*923'; 
+var namagroup17 = 'TESLATOTO';
+var Comment17 = '#TESLATOTO/POLITAS/652*375*923'; 
 
 var namagroup18 = 'CIKA';
 var Comment18 = '#CIKA4D*POTANGINA*760*623*691'; 
