d02a7e30a988584c28ac94713bbcb563c6d90f7f
minochi11 <97766128+minochi11@users.noreply.github.com>
2024-01-01 11:09:26 +0700

    Update BENT4.js

--- BENT V2/BENT4.js
+++ BENT V2/BENT4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW BENT4
 // @namespace    http://tampermonkey.net/
-// @version      3.40
+// @version      3.41
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/BENT%20V2/BENT4.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/BENT%20V2/BENT4.js
@@ -106,8 +106,8 @@ var Comment27 = '#ALEXISTOGEL(DJASKA13) 3D = 909*562*651';
 var namagroup28 = 'LOMBA AMANAH';
 var Comment28 = '#GTATOGEL(DUDUKGO81)*786*275*599'; 
 
-var namagroup29 = 'DASARTOTO';
-var Comment29 = '#DASARTOTO*BOKIKAN*526*479*215'; 
+var namagroup29 = 'BBNI4D';
+var Comment29 = '#(BBNI4D) = (BOKIKAN) 526*479*215'; 
 
 var namagroup30 = 'PORTOGEL';
 var Comment30 = '#PORTOGEL (DINDAMANIS12) = 265*067*130'; 
