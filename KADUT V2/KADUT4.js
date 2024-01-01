5574c587245e7278979ad4dd14035983d4017ce1
minochi11 <97766128+minochi11@users.noreply.github.com>
2024-01-01 11:11:55 +0700

    Update KADUT4.js

--- KADUT V2/KADUT4.js
+++ KADUT V2/KADUT4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW KADUT4
 // @namespace    http://tampermonkey.net/
-// @version      3.39
+// @version      3.40
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/KADUT%20V2/KADUT4.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/KADUT%20V2/KADUT4.js
@@ -105,8 +105,8 @@ var Comment27 = '#ALEXISTOGEL(SANGEKABEH) 3D = 937*249*354';
 var namagroup28 = 'LOMBA AMANAH';
 var Comment28 = '#GTATOGEL(SAWERBOSE0)*537*574*097'; 
 
-var namagroup29 = 'DASARTOTO';
-var Comment29 = '#DASARTOTO*SAWERSEK*287*074*184'; 
+var namagroup29 = 'BBNI4D';
+var Comment29 = '#(BBNI4D) = (SAWERSEK) 287*074*184'; 
 
 var namagroup30 = 'PORTOGEL';
 var Comment30 = '#PORTOGEL (SAWERSEK) = 802*938*832'; 
