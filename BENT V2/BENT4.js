314e7427a4fd248bcabc379976163ca8eddb86fe
minochi11 <97766128+minochi11@users.noreply.github.com>
2024-01-05 19:23:31 +0700

    Update BENT4.js

--- BENT V2/BENT4.js
+++ BENT V2/BENT4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW BENT4
 // @namespace    http://tampermonkey.net/
-// @version      3.42
+// @version      3.43
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/BENT%20V2/BENT4.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/BENT%20V2/BENT4.js
@@ -82,8 +82,8 @@ var Comment19 = '#DETIKTOTO*BORLITA19*110*470*269';
 var namagroup20 = 'PAO4D';
 var Comment20 = '#PAO4D (DUDUKGO81) = 500*239*204'; 
 
-var namagroup21 = 'OBI';
-var Comment21 = '#OBI9*BALUNGAN1*368*010*945*'; 
+var namagroup21 = 'SILATOGEL';
+var Comment21 = '#SILATOGELBETTING (BALUNGAN1) = 368*010*945'; 
 
 var namagroup22 = 'TOGEL838';
 var Comment22 = '#TOGEL838 (DJASKAL12) = 482*898*861'; 
@@ -92,7 +92,7 @@ var namagroup23 = 'ABUTOGEL';
 var Comment23 = '#ABUTOGEL*BOKIKAN*171*715*622'; 
 
 var namagroup24 = 'OTU SLOT';
-var Comment24 = '#OTUSLOT*DJASKAL12*187*795*BETTING'; 
+var Comment24 = '#OTUSLOT*DJASKAL12*187*795*298*BETTING'; 
 
 var namagroup25 = 'BUSURGROUP';
 var Comment25 = commentpasar('#KAPAK4D*BORLITA19*208*406*431'); 
@@ -115,9 +115,8 @@ var Comment30 = '#PORTOGEL (DINDAMANIS12) = 265*067*130';
 var namagroup31 = 'PREDIKSI TOTO';
 var Comment31 = 'BOLER19 # 977/291/211 #YOGAPRO'; 
 
-var namagroup32 = 'SRIGALATOTO';
-var Comment32 = 'SRIGALATOTO*BONES17*371*004*460*BETTING';
-
+var namagroup32 = 'MEGAPRO90';
+var Comment32 = '#MEGAPRO90 ( BONES17 ) = 371*004*460 BETTING';
 
 
 
