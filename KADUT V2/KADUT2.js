7f868f6bd93c96a186bbe8a51d1f67c363e28243
minochi11 <97766128+minochi11@users.noreply.github.com>
2024-01-06 07:11:31 +0700

    Update KADUT2.js

--- KADUT V2/KADUT2.js
+++ KADUT V2/KADUT2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW KADUT2
 // @namespace    http://tampermonkey.net/
-// @version      3.45
+// @version      3.46
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/KADUT%20V2/KADUT2.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/KADUT%20V2/KADUT2.js
@@ -93,8 +93,8 @@ var Comment23 = '#ABUTOGEL*SRIMBELEWEL*522*862*928';
 var namagroup24 = 'OTU SLOT';
 var Comment24 = '#OTUSLOT*SRIWING6*790*584*956*BETTING'; 
 
-var namagroup25 = 'BUSURGROUP';
-var Comment25 = commentpasar('#KAPAK4D*SRILAKCU*658*675*041'); 
+var namagroup25 = 'SILATOGEL';
+var Comment25 = '#SILATOGELBETTING (SRILAKCU) = 658*675*041'; 
 
 var namagroup26 = 'LING77';
 var Comment26 = '#LINGTOGEL77*SRIWING67*949*984*887'; 
@@ -114,9 +114,8 @@ var Comment30 = '#PORTOGEL (SRIMBELEWEL) = 789*163*560';
 var namagroup31 = 'PREDIKSI TOTO';
 var Comment31 = 'SRILAKCU # 885/063/335 #YOGAPRO'; 
 
-var namagroup32 = 'MEGAPRO90';
-var Comment32 = '#MEGAPRO90 ( SRIDU268 ) = 283*860*341 BETTING';
-
+var namagroup32 = 'CRYSTAL';
+var Comment32 = '#CRYSTALTOTO*SRIDU268*283*860*341';
 
 
 
