a4c0c404b7d83df573341faed20a67f6b1707178
minochi11 <97766128+minochi11@users.noreply.github.com>
2024-01-24 23:09:09 +0700

    Update KADUT1.js

--- KADUT V2/KADUT1.js
+++ KADUT V2/KADUT1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW KADUT1
 // @namespace    http://tampermonkey.net/
-// @version      3.47
+// @version      3.48
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/KADUT%20V2/KADUT1.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/KADUT%20V2/KADUT1.js
@@ -19,7 +19,7 @@ var refresh = 90;
 
 
 
-var namagroup1 = 'SHIOKELINCI4D';
+var namagroup1 = 'ShioKelinci';
 var Comment1 = '#shiokelinci4d*BUNDER0*34*94'; 
 
 var namagroup2 = 'PREDIKSI SYAIR';
@@ -103,7 +103,7 @@ var Comment26 = '#LINGTOGEL77*BOLENAN*714*672*164';
 var namagroup27 = 'ALEXISTOGEL';
 var Comment27 = '#ALEXISTOGEL(BUANG31) 3D = 853*834*642'; 
 
-var namagroup28 = 'LOMBA AMANAH';
+var namagroup28 = 'GTATOGEL';
 var Comment28 = '#GTATOGEL(BUNCIT17)*437*093*169'; 
 
 var namagroup29 = 'BBNI4D';
