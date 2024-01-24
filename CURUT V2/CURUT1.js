d26f940c5b34790b030eeec128888b3651aa31cc
minochi11 <97766128+minochi11@users.noreply.github.com>
2024-01-24 23:07:24 +0700

    Update CURUT1.js

--- CURUT V2/CURUT1.js
+++ CURUT V2/CURUT1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW CURUT1
 // @namespace    http://tampermonkey.net/
-// @version      3.18
+// @version      3.19
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/CURUT%20V2/CURUT1.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/CURUT%20V2/CURUT1.js
@@ -20,7 +20,7 @@ var refresh = 90;
 
 
 
-var namagroup1 = 'SHIOKELINCI4D';
+var namagroup1 = 'ShioKelinci';
 var Comment1 = '#shiokelinci4d*QOMAR26*37*94'; 
 
 var namagroup2 = 'PREDIKSI SYAIR';
