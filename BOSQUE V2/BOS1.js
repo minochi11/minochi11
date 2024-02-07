3ddfd2fbfb8b2cd82f0f6c3025a9c19d6cd6d893
minochi11 <97766128+minochi11@users.noreply.github.com>
2024-02-07 11:34:57 +0700

    Update BOS1.js

--- BOSQUE V2/BOS1.js
+++ BOSQUE V2/BOS1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BOS1
 // @namespace    http://tampermonkey.net/
-// @version      3.19
+// @version      3.20
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/BOSQUE%20V2/BOS1.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/BOSQUE%20V2/BOS1.js
@@ -21,7 +21,7 @@ var refresh = 90;
 var namagroup1 = 'ShioKelinci';
 var Comment1 = '#shiokelinci4d*XERBIA13*06*80'; 
 
-var namagroup2 = 'PREDIKSI SYAIR';
+var namagroup2 = 'ELITE';
 var Comment2 = '#IYATOTO XENON88 26*73*41'; 
 
 var namagroup3 = 'TiktakTogel';
