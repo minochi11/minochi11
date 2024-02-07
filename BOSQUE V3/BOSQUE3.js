ab10bf19753acb426ce85bcf1b5633bdf210bc2c
minochi11 <97766128+minochi11@users.noreply.github.com>
2024-02-07 11:37:22 +0700

    Update BOSQUE3.js

--- BOSQUE V3/BOSQUE3.js
+++ BOSQUE V3/BOSQUE3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW BOSQUE3
 // @namespace    http://tampermonkey.net/
-// @version      3.4
+// @version      3.5
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/BOSQUE%20V3/BOSQUE3.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/BOSQUE%20V3/BOSQUE3.js
@@ -19,7 +19,7 @@ var refresh = 90;
 var namagroup1 = 'ShioKelinci4d';
 var Comment1 = '#shiokelinci4d*YONOBO74*92*62'; 
 
-var namagroup2 = 'PREDIKSI SYAIR';
+var namagroup2 = 'ELITE';
 var Comment2 = '#IYATOTO YONOBO74 60*56*34'; 
 
 var namagroup3 = 'TiktakTogel';
