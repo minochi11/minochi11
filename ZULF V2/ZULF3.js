9d18a796072ea0b07d71dee8b58a727aa0c3e515
minochi11 <97766128+minochi11@users.noreply.github.com>
2024-02-09 03:18:10 +0700

    Update ZULF3.js

--- ZULF V2/ZULF3.js
+++ ZULF V2/ZULF3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW ZULF3
 // @namespace    http://tampermonkey.net/
-// @version      3.53
+// @version      3.54
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/ZULF%20V2/ZULF3.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/ZULF%20V2/ZULF3.js
