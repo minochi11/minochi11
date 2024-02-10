17fb26b5f8bbdafb135e7b0ab78c37ab7f18d989
minochi11 <97766128+minochi11@users.noreply.github.com>
2024-02-10 23:53:26 +0700

    Update Yatim2.js

--- YATIM V2/Yatim2.js
+++ YATIM V2/Yatim2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Yatim2
 // @namespace    http://tampermonkey.net/
-// @version      3.53
+// @version      3.54
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/YATIM%20V2/Yatim2.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/YATIM%20V2/Yatim2.js
@@ -18,7 +18,7 @@
 var refresh = 90;
 
 
-var namagroup1 = 'ShioKelinci';
+var namagroup1 = 'SHIOKELINCI';
 var Comment1 = '#shiokelinci4d*FERTULE*43*75'; 
 
 var namagroup2 = 'ELITE';
