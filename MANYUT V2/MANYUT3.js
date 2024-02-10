ce4c349cb71edd318a5a3a60e0bdc754143ff516
minochi11 <97766128+minochi11@users.noreply.github.com>
2024-02-10 23:40:54 +0700

    Update MANYUT3.js

--- MANYUT V2/MANYUT3.js
+++ MANYUT V2/MANYUT3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW MANYUT3
 // @namespace    http://tampermonkey.net/
-// @version      3.55
+// @version      3.57
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/MANYUT%20V2/MANYUT3.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/MANYUT%20V2/MANYUT3.js
