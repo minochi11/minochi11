b9cf07523a2cb23bfbec43e6045500bee6e3d6da
minochi11 <97766128+minochi11@users.noreply.github.com>
2024-02-10 23:54:39 +0700

    Update ZULF2.js

--- ZULF V2/ZULF2.js
+++ ZULF V2/ZULF2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW ZULF2
 // @namespace    http://tampermonkey.net/
-// @version      3.54
+// @version      3.55
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/ZULF%20V2/ZULF2.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/ZULF%20V2/ZULF2.js
@@ -42,7 +42,7 @@ var Comment7 = '#ASEPTOGEL TERMINATO003 18*12*98';
 var namagroup8 = 'BOCORAN PREDIKSI SDY';
 var Comment8 = '#TOGELKUY KORBANSLOT11 05*22*59'; 
 
-var namagroup9 = 'ShioKelinci';
+var namagroup9 = 'SHIOKELINCI';
 var Comment9 = '#shiokelinci4d*BELBIRU12*75*52'; 
 
 var namagroup10 = 'GILA4D';
