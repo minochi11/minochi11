c10acb9adbb1cd700b3ca0de8cb0d73b09a0197d
minochi11 <97766128+minochi11@users.noreply.github.com>
2024-02-11 23:26:16 +0700

    Update BENT4.js

--- BENT V2/BENT4.js
+++ BENT V2/BENT4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW BENT4
 // @namespace    http://tampermonkey.net/
-// @version      3.57
+// @version      3.58
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/BENT%20V2/BENT4.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/BENT%20V2/BENT4.js
@@ -113,7 +113,7 @@ var Comment29 = '(BOKIKAN) = 526*479*215 #tebakangkacola';
 var namagroup30 = 'PORTOGEL';
 var Comment30 = '#PORTOGEL (DINDAMANIS12) = 265*067*130'; 
 
-var namagroup31 = 'PREDIKSI TOTO';
+var namagroup31 = 'YOGA';
 var Comment31 = 'BOLER19 # 977/291/211 #YOGAPRO'; 
 
 var namagroup32 = 'SINAGA4D';
