3d0f7df15179fe221cac89f8466d26f10db5b571
minochi11 <97766128+minochi11@users.noreply.github.com>
2024-02-13 04:09:11 +0700

    Update NEW MENCONG4.js

--- MENCONG V2/NEW MENCONG4.js
+++ MENCONG V2/NEW MENCONG4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW MENCONG4
 // @namespace    http://tampermonkey.net/
-// @version      3.55
+// @version      3.56
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/MENCONG%20V2/NEW%20MENCONG4.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/MENCONG%20V2/NEW%20MENCONG4.js
@@ -46,7 +46,7 @@ var Comment8 = 'ANGKER4D=TOMBIBI24=18*48*51';
 var namagroup9 = 'GILA4D';
 var Comment9 = 'GILA4D=TOMBENG34=18*48*51'; 
 
-var namagroup10 = 'JNETOTO';
+var namagroup10 = 'JNE';
 var Comment10 = '#JNETOTO(TOMSENG21)*72*20*76'; 
 
 var namagroup11 = 'TOTOAKURAT';
