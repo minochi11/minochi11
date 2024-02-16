71e7b526217301704c646f54294706ced62ddb54
minochi11 <97766128+minochi11@users.noreply.github.com>
2024-02-16 23:57:45 +0700

    Update MALBORO1.js

--- MALBORO V2/MALBORO1.js
+++ MALBORO V2/MALBORO1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MALBORO 1
 // @namespace    http://tampermonkey.net/
-// @version      3.56
+// @version      3.57
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/MALBORO%20V2/MALBORO1.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/MALBORO%20V2/MALBORO1.js
