e94138c489a2515036cbb1a02ade9543d0dabcf2
minochi11 <97766128+minochi11@users.noreply.github.com>
2024-02-17 10:38:32 +0700

    Update MALBORO4.js

--- MALBORO V2/MALBORO4.js
+++ MALBORO V2/MALBORO4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MALBORO 4
 // @namespace    http://tampermonkey.net/
-// @version      3.56
+// @version      3.58
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/MALBORO%20V2/MALBORO4.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/MALBORO%20V2/MALBORO4.js
@@ -28,7 +28,7 @@ var Comment3 = '#Tiktaktogel / NARINA90 / 68 , 14 , 81';
 var namagroup4 = 'GAIB4D';
 var Comment4 = 'GAIB4D=NAMIA30=47*45*00'; 
 
-var namagroup5 = '2D OFFICIAL';
+var namagroup5 = 'KEITOGEL';
 var Comment5 = '#keitogel = (NARINA10) = 65*33*21'; 
 
 var namagroup6 = 'KiosToto';
