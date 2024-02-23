0b63579dfe28950ffcc854e18e64e2517f77832a
minochi11 <97766128+minochi11@users.noreply.github.com>
2024-02-23 18:26:50 +0700

    Update KADUT2.js

--- KADUT V2/KADUT2.js
+++ KADUT V2/KADUT2.js
@@ -22,7 +22,7 @@ var refresh = 90;
 var namagroup1 = 'SHIOKELINCI';
 var Comment1 = '#shiokelinci4d*SRIWLIT62*53*40'; 
 
-var namagroup2 = 'ELITE';
+var namagroup2 = 'PREDIKTOR';
 var Comment2 = '#IYATOTO SRIWING72 98*95*76'; 
 
 var namagroup3 = 'TiktakTogel';
