55d1db3f84a14b32ffa6a90b54f3edb84f5f279f
minochi11 <97766128+minochi11@users.noreply.github.com>
2024-02-23 18:29:26 +0700

    Update NEW MENCONG3.js

--- MENCONG V2/NEW MENCONG3.js
+++ MENCONG V2/NEW MENCONG3.js
@@ -22,7 +22,7 @@ var refresh = 90;
 var namagroup1 = 'SHIOKELINCI';
 var Comment1 = '#shiokelinci4d*NOVITASURI12*82*43'; 
 
-var namagroup2 = 'ELITE';
+var namagroup2 = 'PREDIKTOR';
 var Comment2 = '#IYATOTO NOVINOVA231 28*04*91'; 
 
 var namagroup3 = 'TiktakTogel';
