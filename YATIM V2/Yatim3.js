ab97de62dc4952db2e4b8e7f88685bccfb3bed50
minochi11 <97766128+minochi11@users.noreply.github.com>
2024-02-23 16:35:53 +0700

    Update Yatim3.js

--- YATIM V2/Yatim3.js
+++ YATIM V2/Yatim3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Yatim3
 // @namespace    http://tampermonkey.net/
-// @version      3.59
+// @version      3.60
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/YATIM%20V2/Yatim3.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/YATIM%20V2/Yatim3.js
@@ -33,7 +33,7 @@ var Comment4 = 'GAIB4D=SILUWET=21*96*00';
 var namagroup5 = 'KEITOGEL';
 var Comment5 = '#keitogel = (SIMPAK17) = 69*57*34'; 
 
-var namagroup6 = 'KiosToto';
+var namagroup6 = 'KIOSTOTO';
 var Comment6 = 'KIOSTOTO=SINDOLI39=14*65*30'; 
 
 var namagroup7 = 'KHUSUS';
