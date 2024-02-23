77924e70bc2d63a4d7e8ae018e1a38bf45f8a98f
minochi11 <97766128+minochi11@users.noreply.github.com>
2024-02-23 18:25:32 +0700

    Update BOSQUE3.js

--- BOSQUE V3/BOSQUE3.js
+++ BOSQUE V3/BOSQUE3.js
@@ -19,7 +19,7 @@ var refresh = 90;
 var namagroup1 = 'SHIOKELINCI';
 var Comment1 = '#shiokelinci4d*YONOBO74*92*62'; 
 
-var namagroup2 = 'ELITE';
+var namagroup2 = 'PREDIKTOR';
 var Comment2 = '#IYATOTO YONOBO74 60*56*34'; 
 
 var namagroup3 = 'TiktakTogel';
