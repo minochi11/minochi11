dc9c05301fb2fbe667e26a93cd7a808d73398115
minochi11 <97766128+minochi11@users.noreply.github.com>
2024-02-23 18:27:30 +0700

    Update MALBORO1.js

--- MALBORO V2/MALBORO1.js
+++ MALBORO V2/MALBORO1.js
@@ -19,7 +19,7 @@ var refresh = 90;
 var namagroup1 = 'SHIOKELINCI';
 var Comment1 = '#shiokelinci4d*ANANDA122*03*38'; 
 
-var namagroup2 = 'ELITE';
+var namagroup2 = 'PREDIKTOR';
 var Comment2 = '#IYATOTO ANAKCODA83 10*17*97'; 
 
 var namagroup3 = 'TiktakTogel';
