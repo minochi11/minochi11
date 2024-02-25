f7d8387d2cce3fe2227c672c33d8be974483e644
minochi11 <97766128+minochi11@users.noreply.github.com>
2024-02-26 00:01:40 +0700

    Update MALBORO2.js

--- MALBORO V2/MALBORO2.js
+++ MALBORO V2/MALBORO2.js
@@ -64,6 +64,9 @@ var Comment15 = '#VESPATOGEL (BAKULAN12)  74*93*96';
 var namagroup16 = 'hsbshwhs';
 var Comment16 = '';
 
+
+
+
 /*======================================================================3D===============================================================*/
 var namagroup17 = 'TESTBENTO';
 var Comment17 = 'TESTBENTO';
@@ -114,11 +117,15 @@ var namagroup32 = 'TESTBENTO';
 var Comment32 = 'TESTBENTO';
 
 
+
+
+
 var jam = ["Baru sa","1 menit","2 menit","3 menit","4 menit","5 menit","6 menit","7 menit","8 menit","1 mnt","2 mnt","3 mnt","4 mnt","5 mnt","6 mnt","7 mnt","8 mnt"];
 
 
 
 var keyword = ["ROOM", "𝗥𝗢𝗢𝗠", "LOMBA","𝗟𝗢𝗠𝗕𝗔","𝐋𝐎𝐌𝐁𝐀","SENIN","SELASA","RABU","KAMIS","JUMAT","JUMAAT","SABTU","MINGGU","LIMBA","ROM"];
+
 
 var Backlist1 = "GOJEK";
 var Backlist2 = "REKAP";
