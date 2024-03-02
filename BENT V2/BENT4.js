32861248b8079a496696bd183afb80c7e1c37c25
minochi11 <97766128+minochi11@users.noreply.github.com>
2024-03-02 16:22:06 +0700

    Update BENT4.js

--- BENT V2/BENT4.js
+++ BENT V2/BENT4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW BENT4
 // @namespace    http://tampermonkey.net/
-// @version      3.74
+// @version      3.75
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/BENT%20V2/BENT4.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/BENT%20V2/BENT4.js
