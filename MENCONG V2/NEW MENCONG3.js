bf7b9620b89fbb5f99c275fefac66a66077744ef
minochi11 <97766128+minochi11@users.noreply.github.com>
2024-03-02 14:49:27 +0700

    Update NEW MENCONG3.js

--- MENCONG V2/NEW MENCONG3.js
+++ MENCONG V2/NEW MENCONG3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW MENCONG3
 // @namespace    http://tampermonkey.net/
-// @version      3.61
+// @version      3.62
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/MENCONG%20V2/NEW%20MENCONG3.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/MENCONG%20V2/NEW%20MENCONG3.js
@@ -15,7 +15,7 @@
 // ==/UserScript==
 
 
-var refresh = 90;
+var refresh = 50;
 
 
 
