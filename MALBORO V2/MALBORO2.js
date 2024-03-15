55081fcc201a072d669ac1de97d96aec78357f92
minochi11 <97766128+minochi11@users.noreply.github.com>
2024-03-15 19:22:10 +0700

    Update MALBORO2.js

--- MALBORO V2/MALBORO2.js
+++ MALBORO V2/MALBORO2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MALBORO 2
 // @namespace    http://tampermonkey.net/
-// @version      3.66
+// @version      3.67
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/MALBORO%20V2/MALBORO2.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/MALBORO%20V2/MALBORO2.js
@@ -25,7 +25,7 @@ var Comment2 = '#IYATOTO BONCEL58 66*37*18';
 var namagroup3 = 'TiktakTogel';
 var Comment3 = '#Tiktaktogel / BIJILO458 / 29 , 82 , 43'; 
 
-var namagroup4 = 'GAIB4D';
+var namagroup4 = 'G∆IB4D';
 var Comment4 = 'GAIB4D=BOLANGBALING=16*70*34'; 
 
 var namagroup5 = 'KEITOGEL';
@@ -40,13 +40,13 @@ var Comment7 = '#ASEPTOGEL BOGEL438 39*95*62';
 var namagroup8 = 'ANGKER';
 var Comment8 = 'ANGKER4D=BERAK28=29*59*43'; 
 
-var namagroup9 = 'GILA4D';
+var namagroup9 = 'GIL∆4D';
 var Comment9 = 'GILA4D=BAJUKU34=39*95*62'; 
 
 var namagroup10 = 'JNE';
 var Comment10 = '#JNETOTO(BANANA12)*07*28*84'; 
 
-var namagroup11 = 'A_K_U_R_A_T';
+var namagroup11 = 'T_O_T_O_A_K_U_R_A_T';
 var Comment11 = '#TA*BAJIGUR45*85*51*40*'; 
 
 var namagroup12 = 'BOCORAN PREDIKSI SDY';
@@ -61,8 +61,8 @@ var Comment14 = '#DENTOTO 74*93*96 ( BAKEKLO )';
 var namagroup15 = 'SNIPER NUSANTARA';
 var Comment15 = '#VESPATOGEL (BAKULAN12)  74*93*96'; 
 
-var namagroup16 = 'hsbshwhs';
-var Comment16 = '';
+var namagroup16 = '123';
+var Comment16 = '';"
 
 
 
