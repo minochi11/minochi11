600ef1d4952f4f237fb94dbde9b52a56cbcdbcc2
minochi11 <97766128+minochi11@users.noreply.github.com>
2024-03-16 01:39:38 +0700

    Update BENT3.js

--- BENT V2/BENT3.js
+++ BENT V2/BENT3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW BENT3
 // @namespace    http://tampermonkey.net/
-// @version      3.77
+// @version      3.78
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/BENT%20V2/BENT3.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/BENT%20V2/BENT3.js
@@ -19,7 +19,7 @@ var refresh = 90;
 
 
 
-var namagroup1 = 'PREDIKTOR';
+var namagroup1 = 'HOKONG';
 var Comment1 = '#IYATOTO CINOIRENG12 39*53*73'; 
 
 var namagroup2 = 'ANGKER';
