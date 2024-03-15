830f1fcab516b858c0648a676a075d6296a858db
minochi11 <97766128+minochi11@users.noreply.github.com>
2024-03-15 19:20:13 +0700

    Update KADUT2.js

--- KADUT V2/KADUT2.js
+++ KADUT V2/KADUT2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW KADUT2
 // @namespace    http://tampermonkey.net/
-// @version      3.75
+// @version      3.76
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/KADUT%20V2/KADUT2.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/KADUT%20V2/KADUT2.js
@@ -28,7 +28,7 @@ var Comment2 = '#IYATOTO SRIWING72 98*95*76';
 var namagroup3 = 'TiktakTogel';
 var Comment3 = '#Tiktaktogel / SRIMIT64 / 79 , 36 , 61'; 
 
-var namagroup4 = 'GAIB4D';
+var namagroup4 = 'G∆IB4D';
 var Comment4 = 'GAIB4D=SRIWET67=79*36*61'; 
 
 var namagroup5 = 'KEITOGEL';
@@ -37,19 +37,19 @@ var Comment5 = '#keitogel = (SRIANU36) = 15*64*16';
 var namagroup6 = 'KIOSTOTO';
 var Comment6 = 'KIOSTOTO=SRIWIDURTI=55*78*67'; 
 
-var namagroup7 = 'KHUSUS MACAU';
+var namagroup7 = 'KHUSUS';
 var Comment7 = '#ASEPTOGEL SRINDIM63 15*64*16'; 
 
 var namagroup8 = 'ANGKER';
 var Comment8 = 'ANGKER4D=SRINDIM63=03*27*96'; 
 
-var namagroup9 = 'GILA4D';
+var namagroup9 = 'GIL∆4D';
 var Comment9 = 'GILA4D=SRIWEL74=03*27*96'; 
 
 var namagroup10 = 'JNE';
 var Comment10 = '#JNETOTO(SRIWING67)*03*27*96'; 
 
-var namagroup11 = 'A_K_U_R_A_T';
+var namagroup11 = 'T_O_T_O_A_K_U_R_A_T';
 var Comment11 = '#TA*SRIANU36*55*78*67*'; 
 
 var namagroup12 = 'BOCORAN PREDIKSI SDY';
@@ -58,14 +58,14 @@ var Comment12 = '#TOGELKUY SRISUWING 93*81*45';
 var namagroup13 = 'TOYIBSLOT';
 var Comment13 = '#TOYIBSLOT ( SRIMBELEWEL ) : 98*95*76'; 
 
-var namagroup14 = 'IDTOGEL';
-var Comment14 = '#IDTOGEL*SRITILKON*93*81*45'; 
+var namagroup14 = 'DENTOTO';
+var Comment14 = '#DENTOTO 93*81*45 ( SRILAKCU )'; 
 
-var namagroup15 = 'SNIPER';
-var Comment15 = '#VESPATOGEL (SRILAKCU) 75*07*04'; 
+var namagroup15 = 'SNIPER NUSANTARA';
+var Comment15 = '#VESPATOGEL (SRILAKCU)  75*07*04'; 
 
-var namagroup16 = 'DENTOTO';
-var Comment16 = '#DENTOTO 93*81*45 ( SRILAKCU )';
+var namagroup16 = '123123123';
+var Comment16 = '';
 
 
 /*======================================================================3D===============================================================*/
