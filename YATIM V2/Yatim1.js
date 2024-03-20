29435f03780c2fffb18f051baa30c4f2a20bc46e
minochi11 <97766128+minochi11@users.noreply.github.com>
2024-03-20 13:04:07 +0700

    Update Yatim1.js

--- YATIM V2/Yatim1.js
+++ YATIM V2/Yatim1.js
@@ -60,7 +60,7 @@ var Comment13 = '#TOYIBSLOT ( GUNGYIBS ) : 49*01*47';
 var namagroup14 = 'DENTOTO';
 var Comment14 = '#DENTOTO 15*56*90 ( GUNGSIPP )'; 
 
-var namagroup15 = 'VESPA';
+var namagroup15 = 'DEWA';
 var Comment15 = '#VESPATOGEL (GUNGSNIPE)  49*01*47'; 
 
 var namagroup16 = '1231';
