b28c3887c0add4c5b22a00a1dc64c6a35da6c91b
minochi11 <97766128+minochi11@users.noreply.github.com>
2024-03-23 20:35:56 +0700

    Update MANUK 3.js

--- MANUK/MANUK 3.js
+++ MANUK/MANUK 3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MANUK 3
 // @namespace    http://tampermonkey.net/
-// @version      3.75
+// @version      3.76
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/MANUK/MANUK%203.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/MANUK/MANUK%203.js
@@ -20,7 +20,7 @@ var refresh = 50;
 var namagroup1 = 'SHIOKELINCI';
 var Comment1 = '#shiokelinci4d*DOROKERTO*20*71'; 
 
-var namagroup2 = 'ANGKA JP';
+var namagroup2 = 'MARKAS';
 var Comment2 = '#IYATOTO DOROMIBER 29*52*30'; 
 
 var namagroup3 = 'TiktakTogel';
