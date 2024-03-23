fba2cd5e0af13aeab67f6fc4cd77bd95bc0d0797
minochi11 <97766128+minochi11@users.noreply.github.com>
2024-03-23 20:32:26 +0700

    Update BOSQUE4.js

--- BOSQUE V3/BOSQUE4.js
+++ BOSQUE V3/BOSQUE4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW BOSQUE4
 // @namespace    http://tampermonkey.net/
-// @version      3.21
+// @version      3.22
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/BOSQUE%20V3/BOSQUE4.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/BOSQUE%20V3/BOSQUE4.js
