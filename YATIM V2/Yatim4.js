4b59e850be27acba383f4a42d0710a7e7dec1b82
minochi11 <97766128+minochi11@users.noreply.github.com>
2024-03-24 15:19:11 +0700

    Update Yatim4.js

--- YATIM V2/Yatim4.js
+++ YATIM V2/Yatim4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Yatim4
 // @namespace    http://tampermonkey.net/
-// @version      3.70
+// @version      3.71
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/YATIM%20V2/Yatim4.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/YATIM%20V2/Yatim4.js
