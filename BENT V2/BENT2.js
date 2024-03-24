8dc543d6fe8d5321ebece11c49e4b13840834517
minochi11 <97766128+minochi11@users.noreply.github.com>
2024-03-24 16:28:06 +0700

    Update BENT2.js

--- BENT V2/BENT2.js
+++ BENT V2/BENT2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW BENT2
 // @namespace    http://tampermonkey.net/
-// @version      3.86
+// @version      3.87
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/BENT%20V2/BENT2.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/BENT%20V2/BENT2.js
@@ -103,7 +103,7 @@ var Comment26 = '#LINGTOGEL77*BIKANG12*073*596*003';
 var namagroup27 = 'AL3XISTOG3L';
 var Comment27 = '#ALEXISTOGEL(BONANSA67) 3D = 718*828*418'; 
 
-var namagroup28 = 'Kiutoto';
+var namagroup28 = 'KIUTOTO';
 var Comment28 = '#KIUTOTO (BINAKARMA54) : 751*389*494'; 
 
 var namagroup29 = 'AKIRA';
