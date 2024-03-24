572669a1e89715bc1ff46ffe04071f92a78b4d95
minochi11 <97766128+minochi11@users.noreply.github.com>
2024-03-24 15:17:56 +0700

    Update NEW MENCONG2.js

--- MENCONG V2/NEW MENCONG2.js
+++ MENCONG V2/NEW MENCONG2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW MENCONG2
 // @namespace    http://tampermonkey.net/
-// @version      3.71
+// @version      3.72
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/MENCONG%20V2/NEW%20MENCONG2.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/MENCONG%20V2/NEW%20MENCONG2.js
@@ -21,7 +21,7 @@ var namagroup1 = 'SHIOKELINCI';
 var Comment1 = '#shiokelinci4d*KRISWATI89*09*99'; 
 
 var namagroup2 = 'MARKAS';
-var Comment2 = '#IYATOTO KRISAKTI1 06*29*21'; 
+var Comment2 = 'IYATOTO KRISAKTI1 06*29*21'; 
 
 var namagroup3 = 'TiktakTogel';
 var Comment3 = '#Tiktaktogel / KRISMANDA23 / 70 , 87 , 80'; 
