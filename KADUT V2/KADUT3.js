2dea1116a045e33b8b4bf8cb0e3014ff0c89e8d5
minochi11 <97766128+minochi11@users.noreply.github.com>
2024-03-26 23:11:05 +0700

    Update KADUT3.js

--- KADUT V2/KADUT3.js
+++ KADUT V2/KADUT3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW KADUT3
 // @namespace    http://tampermonkey.net/
-// @version      3.88
+// @version      3.89
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/minochi11/minochi11/main/KADUT%20V2/KADUT3.js
 // @downloadURL  https://raw.githubusercontent.com/minochi11/minochi11/main/KADUT%20V2/KADUT3.js
@@ -15,7 +15,7 @@
 // ==/UserScript==
 
 
-var refresh = 90;
+var refresh = 50;
 
 
 
@@ -28,7 +28,7 @@ var Comment2 = 'IYATOTO JOMPLANG 56*26*54';
 var namagroup3 = 'TiktakTogel';
 var Comment3 = '#Tiktaktogel / JONTOR6 / 87 , 41 , 20'; 
 
-var namagroup4 = 'G∆IB4D';
+var namagroup4 = 'GA1B4D';
 var Comment4 = 'GAIB4D=JOKOBODO=87*41*20'; 
 
 var namagroup5 = 'KEITOGEL';
@@ -52,8 +52,8 @@ var Comment10 = '#JNETOTO(JOMNDUL73)*46*72*49';
 var namagroup11 = 'T_O_T_O_A_K_U_R_A_T';
 var Comment11 = '#TA*JONTOR64*05*29*44*'; 
 
-var namagroup12 = 'BOCORAN PREDIKSI SDY';
-var Comment12 = '#TOGELKUY JOJONBETAWI 70*31*86'; 
+var namagroup12 = 'MASTER KUY';
+var Comment12 = 'TOGELKUY JOJONBETAWI 70*31*86'; 
 
 var namagroup13 = 'TOYIBSLOT';
 var Comment13 = '#TOYIBSLOT ( JONGONO ) : 56*26*54'; 
@@ -61,8 +61,8 @@ var Comment13 = '#TOYIBSLOT ( JONGONO ) : 56*26*54';
 var namagroup14 = 'DENTOTO';
 var Comment14 = '#DENTOTO 70*31*86 ( JOROKLUH )'; 
 
-var namagroup15 = 'DEWA';
-var Comment15 = '#VESPATOGEL (JOROKLUH)  32*28*01'; 
+var namagroup15 = 'HATRICK';
+var Comment15 = 'VESPATOGEL (JOROKLUH)  32*28*01'; 
 
 var namagroup16 = '12312312';
 var Comment16 = '';
